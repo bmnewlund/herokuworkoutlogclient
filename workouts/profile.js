18d1d98f2ec5ace3f2f0e0db1548a9a389746448
Brett Newlund <bmnewlund@gmail.com>
2017-09-12 16:51:42 -0400

    added HTML changes

--- workouts/profile.js
+++ workouts/profile.js
@@ -5,5 +5,32 @@ $(function() {
 
 			create: function() {
 
-				var 
-			}
+				var prof = {
+						firstName: $("#prof-firstname"),
+						lastName: $("#prof-lastname"),
+						age: $("#prof-age");
+						weight: $("#prof-weight"),
+						height: $("#prof-height"),
+				};
+				var postData = { profile: prof };
+				var profileCreate = $.ajax({
+
+					type: "POST",
+					url: WorkoutLog.API_BASE + "profile",
+					data: JSON.stringify(postData),
+					contentType: "application/json"
+				});
+
+				profileCreate.done(function(data) {
+						WorkoutLog.profile.userProfiles.push(data.definition);
+						$("#prof-firstname").val("");
+						$("#prof-lastname").val("");
+						$("#prof-age").val("");
+						$("#prof-weight").val("");
+						$("#prof-height").val("");
+					});
+				}
+
+
+					// bindings
+		$("#prof-save").on("click", WorkoutLog.profile.create);
